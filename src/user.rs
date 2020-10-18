a238dff2eb6e8455efa2003435710a2f22f988b5
Malhar Vora <mlvora.2010@gmail.com>
2020-10-18 10:09:55 -0700

    Add Overall Ranks in User struct

--- src/user.rs
+++ src/user.rs
@@ -1,6 +1,38 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct OverallRank {
+    pub rank: i32,
+    pub name: String,
+    pub color: String,
+    pub score: u32,
+}
+
+impl OverallRank {
+    pub fn new() -> Self {
+        OverallRank {
+            rank: 0,
+            name: "".to_string(),
+            color: "".to_string(),
+            score: 0,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Ranks {
+    pub overall: OverallRank,
+}
+
+impl Ranks {
+    pub fn new() -> Self {
+        Ranks {
+            overall: OverallRank::new(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct CodeChallenges {
     pub total_authored: u64,
     pub total_completed: u64,
@@ -23,6 +55,7 @@ pub struct User {
     pub clan: Option<String>,
     pub skills: Vec<String>,
     pub code_challenges: CodeChallenges,
+    pub ranks: Ranks,
 }
 
 impl User {
@@ -34,6 +67,7 @@ impl User {
             clan: None,
             skills: vec![],
             code_challenges: CodeChallenges::new(),
+            ranks: Ranks::new(),
         }
     }
 }

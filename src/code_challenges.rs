4447069523efd7d846119b20d995fd2f30b80855
Malhar Vora <mlvora.2010@gmail.com>
2020-10-26 10:01:01 -0700

    Add method to create new instance of CodeChallenge and related structs

--- src/code_challenges.rs
+++ src/code_challenges.rs
@@ -4,15 +4,43 @@ pub struct Rank {
     pub color: String,
 }
 
+impl Rank {
+    pub fn new() -> Self {
+        Rank {
+            id: 0,
+            name: "".to_string(),
+            color: "".to_string(),
+        }
+    }
+}
+
 // Also used to store ApprovedBy details
 pub struct CreatedBy {
     pub username: String,
     pub url: String,
 }
 
+impl CreatedBy {
+    pub fn new() -> Self {
+        CreatedBy {
+            username: "".to_string(),
+            url: "".to_string(),
+        }
+    }
+}
+
 pub struct Unresolved {
     pub issues: u64,
     pub suggestions: u64,
+}
+
+impl Unresolved {
+    pub fn new() -> Self {
+        Unresolved {
+            issues: 0,
+            suggestions: 0,
+        }
+    }
 }
 
 pub struct CodeChallenge {
@@ -36,4 +64,31 @@ pub struct CodeChallenge {
     pub tags: Vec<String>,
     pub contributors_wanted: bool,
     pub unresolved: Unresolved,
+}
+
+impl CodeChallenge {
+    pub fn new() -> Self {
+        CodeChallenge {
+            id: "".to_string(),
+            name: "".to_string(),
+            slug: "".to_string(),
+            category: "".to_string(),
+            published_at: "".to_string(),
+            approved_at: "".to_string(),
+            languages: vec![],
+            url: "".to_string(),
+            rank: Rank::new(),
+            created_at: "".to_string(),
+            created_by: CreatedBy::new(),
+            approved_by: CreatedBy::new(),
+            description: "".to_string(),
+            total_attempts: 0,
+            total_completed: 0,
+            total_stars: 0,
+            vote_scores: 0,
+            tags: vec![],
+            contributors_wanted: false,
+            unresolved: Unresolved::new(),
+        }
+    }
 }
